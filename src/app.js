9086578d4ee4535f35fe22e682ddefbc4bcb9427
hectorstudio <david.liu987@hotmail.com>
2019-11-21 11:56:57 +0000

    Enable CORS

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import router from './routes';
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
