ee38a80ad101c0cf23c13b10a87003ab946ab2cd
hectorstudio <david.liu987@hotmail.com>
2019-11-26 02:25:45 +0000

    Update location of firebase account key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 import * as admin from 'firebase-admin';
 
-import serviceAccount from './firebase_account_key.json';
+import serviceAccount from '../../firebase_account_key.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
