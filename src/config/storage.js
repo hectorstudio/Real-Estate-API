6cf286d0d3f47fef61e6efc30affd634db60824c
hectorstudio <david.liu987@hotmail.com>
2019-11-28 18:42:25 +0000

    Disable no-unresolved for the whole storage config file

--- src/config/storage.js
+++ src/config/storage.js
@@ -1,8 +1,9 @@
+// On CI, this file is missing and eslint can throw an error
+/* eslint-disable import/no-unresolved */
+
 import { Storage } from '@google-cloud/storage';
 import path from 'path';
 
-// On CI, this file is missing and eslint can throw an error
-// eslint-disable-next-line import/no-unresolved
 const serviceAccountKey = path.resolve(__dirname, '../../firebase_account_key.json');
 
 const storage = new Storage({ keyFilename: serviceAccountKey });
