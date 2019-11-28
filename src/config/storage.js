1d025ed5963b458dd276a73446bcbe650006bace
hectorstudio <david.liu987@hotmail.com>
2019-11-28 18:39:25 +0000

    Eslint disable firebase service account key import line

--- src/config/storage.js
+++ src/config/storage.js
@@ -1,6 +1,8 @@
 import { Storage } from '@google-cloud/storage';
 import path from 'path';
 
+// On CI, this file is missing and eslint can throw an error
+// eslint-disable-next-line import/no-unresolved
 const serviceAccountKey = path.resolve(__dirname, '../../firebase_account_key.json');
 
 const storage = new Storage({ keyFilename: serviceAccountKey });
