87bc6e3e88e29244b2b5729d356146a8182f208e
hectorstudio <david.liu987@hotmail.com>
2019-12-04 13:28:12 +0000

    Include buildings routes in app router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import usersRouter from './users';
 import filesRouter from './files';
+import buildingsRouter from './buildings';
 import notFound from './notFound';
 import errorHandler from './errorHandler';
 
 export default (app) => app
-  .use('/users', usersRouter)
+  .use('/buildings', buildingsRouter)
   .use('/files', filesRouter)
+  .use('/users', usersRouter)
   .use(notFound)
   .use(errorHandler);
